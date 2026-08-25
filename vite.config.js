256d710e4e2ee4c30214816df2f47cea6485d490
Neeraj chauhan <neerajchauhangvr@gmail.com>
2026-08-26 01:07:57 +1200

    Make deployment path configurable

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/avcena.github.io/",
+  base: process.env.VITE_BASE_PATH || "/avcena.github.io/",
   server: {
     proxy: {
       "/api": "http://localhost:3001"
